0b1cda9a47dde14474b01f092910fdd0a07a04c8
kyusoo <rbtn110@uengine.org>
2024-05-27 16:48:04 +0900

    field 조건문 추가

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -147,8 +147,8 @@ public class {{namePascalCase}}Test {
    for(var i = 0; i < this.aggregateList.length; i++){
       givenField = this.aggregateList[0].aggregateRoot.fieldDescriptors;
    }
-   var whenField = this.incomingRelations[0].source.fieldDescriptors;
-   var thenField = this.outgoingRelations[0].target.fieldDescriptors;
+   if(this.incomingRelations)var whenField = this.incomingRelations[0].source.fieldDescriptors;
+   if(this.outgoingRelations)var thenField = this.outgoingRelations[0].target.fieldDescriptors;
 
    window.$HandleBars.registerHelper('toJava', convertToJavaSyntax)
 
