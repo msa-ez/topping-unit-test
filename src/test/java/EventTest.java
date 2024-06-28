69cdeebfaa045c5a9f14a05638f7852a9d91b582
kyusoo <rbtn110@uengine.org>
2024-06-28 16:54:45 +0900

    topping 수정

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,6 +1,7 @@
 forEach: Policy
 fileName: {{namePascalCase}}Test.java
 path: {{boundedContext.name}}/src/test/java/{{options.package}}
+except: {{#checkExamples examples}}{{/checkExamples}}
 ---
 
 package {{options.package}};
@@ -68,7 +69,7 @@ public class {{namePascalCase}}Test {
 
    {{#given}}
    {{#each value}}
-      entity.set{{pascalCase @key}}({{{toJava this}}});
+      entity.set{{pascalCase @key}}({{setGivenField @key this}});
    {{/each}}
    {{/given}}
 
@@ -82,7 +83,7 @@ public class {{namePascalCase}}Test {
 
    {{#when}}
    {{#each value}}
-      event.set{{pascalCase @key}}({{{toJava this}}});
+      event.set{{pascalCase @key}}({{setWhenField @key this}});
    {{/each}}
    {{/when}}
    
@@ -125,7 +126,7 @@ public class {{namePascalCase}}Test {
 
       {{#then}}
       {{#each value}}
-         assertEquals(outputEvent.get{{pascalCase @key}}(), {{{toJava this}}});
+         assertEquals(outputEvent.get{{pascalCase @key}}(), {{setThenField @key this}});
       {{/each}}
       {{/then}}
 
@@ -142,41 +143,118 @@ public class {{namePascalCase}}Test {
 }
 
 <function>
+   var givenField = []
+   for(var i = 0; i < this.aggregateList.length; i++){
+      givenField = this.aggregateList[0].aggregateRoot.fieldDescriptors;
+   }
+   if(this.incomingRelations){
+      var whenField = this.incomingRelations[0].source.fieldDescriptors;
+   }
+   if(this.outgoingRelations){
+      var thenField = this.outgoingRelations[0].target.fieldDescriptors;
+   }
 
-window.$HandleBars.registerHelper('toJava', convertToJavaSyntax)
+   window.$HandleBars.registerHelper('toJava', convertToJavaSyntax)
 
-function convertToJavaSyntax(value) {
-  const type = typeof value;
+   function convertToJavaSyntax(value) {
+     const type = typeof value;
+   
+     switch (type) {
+       case 'string':
+         return `"${value}"`; // Java에서 문자열은 큰따옴표를 사용합니다.
+       case 'number':
+         // JavaScript의 숫자는 정수 또는 부동소수점일 수 있으므로 이를 구분해야 할 수도 있습니다.
+         if (Number.isSafeInteger(value)) {
+           return `${value}L`; // long 타입으로 간주할 수 있습니다.
+         } else {
+           return `${value}D`; // double 타입으로 간주할 수 있습니다.
+         }
+       case 'boolean':
+         return value.toString();
+       case 'object':
+         if (value instanceof Date) {
+           return `new Date(${value.getTime()}L)`; // Java의 Date 생성자를 사용합니다.
+         } else if (value === null) {
+           return 'null';
+         } else if (Array.isArray(value)) {
+           // 배열의 경우 더 복잡한 로직이 필요할 수 있으며, 이는 예시로만 제공됩니다.
+           const elements = value.map((element) => convertToJavaSyntax(element)).join(', ');
+           return `new Object[]{${elements}}`; // Object 배열로 간주합니다.
+         } else {
+           // 다른 종류의 객체에 대한 처리가 필요할 수 있습니다.
+           // 이 경우 해당 객체를 적절한 Java 표현으로 변환하는 로직이 필요합니다.
+           return value.toString(); // 기본적인 toString 반환을 사용합니다.
+         }
+       default:
+         throw new Error(`Unsupported type: ${type}`);
+     }
+   }
 
-  switch (type) {
-    case 'string':
-      return `"${value}"`; // Java에서 문자열은 큰따옴표를 사용합니다.
-    case 'number':
-      // JavaScript의 숫자는 정수 또는 부동소수점일 수 있으므로 이를 구분해야 할 수도 있습니다.
-      if (Number.isSafeInteger(value)) {
-        return `${value}L`; // long 타입으로 간주할 수 있습니다.
-      } else {
-        return `${value}D`; // double 타입으로 간주할 수 있습니다.
+window.$HandleBars.registerHelper('checkExamples', function (examples) {
+   if(!examples)return true
+});
+
+window.$HandleBars.registerHelper('setGivenField', function (key, value) {
+   var type = 'String';
+   for(var i = 0; i < givenField.length; i++){
+      if(givenField[i].name == key){
+         type = givenField[i].className
       }
-    case 'boolean':
+   }
+   switch (type) {
+      case 'String':
+         return "\"" + value + "\"" // Java에서 문자열은 큰따옴표를 사용합니다.
+      case 'Long':
+         return `${value}L`;
+      case 'Integer':
+         return `${value}`;
+      case 'Boolean':
       return value.toString();
-    case 'object':
-      if (value instanceof Date) {
-        return `new Date(${value.getTime()}L)`; // Java의 Date 생성자를 사용합니다.
-      } else if (value === null) {
-        return 'null';
-      } else if (Array.isArray(value)) {
-        // 배열의 경우 더 복잡한 로직이 필요할 수 있으며, 이는 예시로만 제공됩니다.
-        const elements = value.map((element) => convertToJavaSyntax(element)).join(', ');
-        return `new Object[]{${elements}}`; // Object 배열로 간주합니다.
-      } else {
-        // 다른 종류의 객체에 대한 처리가 필요할 수 있습니다.
-        // 이 경우 해당 객체를 적절한 Java 표현으로 변환하는 로직이 필요합니다.
-        return value.toString(); // 기본적인 toString 반환을 사용합니다.
-      }
-    default:
+      default:
       throw new Error(`Unsupported type: ${type}`);
-  }
-}
+   }
+});
+
+window.$HandleBars.registerHelper('setWhenField', function (key, value) {
+   var type = 'String';
+   for(var i = 0; i < whenField.length; i++){
+      if(whenField[i].name == key){
+         type = whenField[i].className
+      }
+   }
+   switch (type) {
+      case 'String':
+         return "\"" + value + "\"" // Java에서 문자열은 큰따옴표를 사용합니다.
+      case 'Long':
+         return `${value}L`;
+      case 'Integer':
+         return `${value}`;
+      case 'Boolean':
+      return value.toString();
+      default:
+      throw new Error(`Unsupported type: ${type}`);
+   }
+});
+window.$HandleBars.registerHelper('setThenField', function (key, value) {
+   var type = 'String';
+   for(var i = 0; i < thenField.length; i++){
+      if(thenField[i].name == key){
+         type = thenField[i].className
+      }
+   }
+   switch (type) {
+      case 'String':
+         return "\"" + value + "\"" // Java에서 문자열은 큰따옴표를 사용합니다.
+      case 'Long':
+         return `${value}L`;
+      case 'Integer':
+         return `${value}`;
+      case 'Boolean':
+      return value.toString();
+      default:
+      throw new Error(`Unsupported type: ${type}`);
+   }
+});
+
 
 </function>
