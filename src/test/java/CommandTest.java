21a0f72c7c809a79f46dc46ad075b8cb1a60b290
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-11-29 12:08:48 +0900

    test exception 수정

--- src/test/java/CommandTest.java
+++ src/test/java/CommandTest.java
@@ -1,6 +1,7 @@
 forEach: Command
 fileName: {{namePascalCase}}Test.java
 path: {{boundedContext.name}}/src/test/java/{{options.package}}
+except: {{#checkExamples examples}}{{/checkExamples}}
 ---
 
 package {{options.package}};
